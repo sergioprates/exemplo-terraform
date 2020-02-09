f14b583fec06937e2509f9363806da83c50bba68
sergio.prates <sergio.prates@lambda3.com.br>
2020-02-09 14:14:24 -0300

    removido comentários

--- terraform-aws/deploy.tf
+++ terraform-aws/deploy.tf
@@ -48,12 +48,9 @@ resource "aws_security_group" "permitirAcesso" {
   description = "Permitir acesso ao banco"
 
   ingress {
-    # TLS (change to whatever ports you need)
     from_port   = 3306 
     to_port     = 3306
     protocol    = "tcp"
-    # Please restrict your ingress to only necessary IPs and ports.
-    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
     cidr_blocks     = ["0.0.0.0/0"]
   }
 
